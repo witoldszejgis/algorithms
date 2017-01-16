1af15bc3c85c89f93f394038f9568c72ceba00cd
Witold Szejgis <witoldszejgis@Witolds-MacBook-Pro.local>
2017-01-16 16:49:52 +0100

    binarySearchTree.py   added

---
+++ binarySearchTree.py
@@ -0,0 +1,20 @@
+""" Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+"""
+//CHecking if given tree is proper BST
+def isValidBST(node, MIN, MAX):
+    if(node == None):
+        return True;
+    if(node.data > MIN and node.data < MAX and \
+       isValidBST(node.left, MIN, node.data) and \
+       isValidBST(node.right, node.data, MAX)):
+          return True;
+    else:
+         return False;
+
+def check_binary_search_tree_(root):
+    return isValidBST(root,-100000,100000)
